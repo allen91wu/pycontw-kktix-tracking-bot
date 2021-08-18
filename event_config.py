cd3ca74dc8fa3975acd7e76b44ebc9609787aef8
allen91wu <allen91.wu@gmail.com>
2021-08-19 00:59:20 +0800

    feat(main.py): use event_config to maintain ticket type and link

---
+++ event_config.py
@@ -0,0 +1,12 @@
+"""
+For user to setting the event config.
+"""
+# Your event name
+EVENT_NAME = "PyCon TW 2021"
+# The ticket type and kktix pages link
+EVENT_TICKET = {
+    "個人票": "https://pycontw.kktix.cc/events/2021-individual",
+    "企業票": "https://pycontw.kktix.cc/events/2021-corporate",
+    "保留票": "https://pycontw.kktix.cc/events/2021-reserved",
+    "Sprint": "https://pycontw.kktix.cc/events/20210926-sprints",
+}
