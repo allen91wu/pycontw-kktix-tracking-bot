cc99ee9bed97ea84977de309ba6a0bb4b410c51e
allen91wu <allen91.wu@gmail.com>
2021-08-30 16:58:07 +0800

    docs(event_config.py): update annotation

--- event_config.py
+++ event_config.py
@@ -3,13 +3,15 @@ For user to setting the event config.
 """
 # Event name
 EVENT_NAME = "PyCon TW 2021"
-# The ticket type and kktix pages link
+# The ticket type and kktix pages link for main event
 MAIN_EVENT_TICKET = {
     "個人票": "https://pycontw.kktix.cc/events/2021-individual",
     "企業票": "https://pycontw.kktix.cc/events/2021-corporate",
     "保留票": "https://pycontw.kktix.cc/events/2021-reserved",
 }
+# The target of main event quantity of tickets
 MAIN_TICKET_TARGET = "800"
+# The ticket type and kktix pages link for child event
 CHILD_EVENT_TICKET = {
     "Sprint": "https://pycontw.kktix.cc/events/20210926-sprints",
 }
