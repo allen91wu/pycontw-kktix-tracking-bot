2e5738bd34166ac3f3009b3ac98a5cbc02822a8d
allen91wu <allen91.wu@gmail.com>
2021-08-30 16:53:03 +0800

    feat(main.py): add total quantity

--- event_config.py
+++ event_config.py
@@ -4,9 +4,12 @@ For user to setting the event config.
 # Event name
 EVENT_NAME = "PyCon TW 2021"
 # The ticket type and kktix pages link
-EVENT_TICKET = {
+MAIN_EVENT_TICKET = {
     "個人票": "https://pycontw.kktix.cc/events/2021-individual",
     "企業票": "https://pycontw.kktix.cc/events/2021-corporate",
     "保留票": "https://pycontw.kktix.cc/events/2021-reserved",
+}
+MAIN_TICKET_TARGET = "800"
+CHILD_EVENT_TICKET = {
     "Sprint": "https://pycontw.kktix.cc/events/20210926-sprints",
 }
--- main.py
+++ main.py
@@ -8,7 +8,12 @@ from bs4 import BeautifulSoup
 from discord import channel  # noqa: F401
 from discord.ext import commands
 from dotenv import load_dotenv
-from event_config import EVENT_NAME, EVENT_TICKET
+from event_config import (
+    EVENT_NAME,
+    MAIN_EVENT_TICKET,
+    CHILD_EVENT_TICKET,
+    MAIN_TICKET_TARGET,
+)
 
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
@@ -18,28 +23,41 @@ NOTIFY_TIME = os.getenv("NOTIFY_TIME", default="Sunday-14:00")
 client = commands.Bot(command_prefix="!")
 
 
-def kktix_count(web):
+def kktix_count(display, web):
     r = requests.get(web)
     soup = BeautifulSoup(r.text, "html.parser")
     ticket_count = soup.find("span", class_="info-count")
     if ticket_count:
-        return ticket_count.text
+        if display == "on":
+            return ticket_count.text
+        if display == "off":
+            return int(ticket_count.text.split("/")[0])
     else:
         return "N/A"
 
 
 def event_ticket_count():
-    count_data = ""
-    for ticket in EVENT_TICKET.items():
-        ticket_count = f"{ticket[0]}：{kktix_count(ticket[1])}"
-        count_data += f"{ticket_count}\n"
-    return count_data
+    main_ticket_count = "-------------- **主活動** --------------\n"
+    child_ticket_count = "-------------- **子活動** --------------\n"
+    main_total = 0
+
+    for ticket in MAIN_EVENT_TICKET.items():
+        status_count = kktix_count("off", ticket[1])
+        main_total += status_count
+        main_ticket_count += f"- {ticket[0]}：{status_count}\n"
+    main_ticket_count += f"**總售票人數：{main_total} / {MAIN_TICKET_TARGET}\n**"
+
+    for ticket in CHILD_EVENT_TICKET.items():
+        status_count = kktix_count("on", ticket[1])
+        child_ticket_count += f"- {ticket[0]}：{status_count}\n"
+
+    return main_ticket_count + child_ticket_count
 
 
 @client.command()
 async def kktix_status(ctx):
     await ctx.message.delete()
-    msg = f"{EVENT_NAME} 目前售票狀況為：\n{event_ticket_count()}"
+    msg = f"**{EVENT_NAME}** 目前售票狀況：\n{event_ticket_count()}"
     await ctx.send(msg)
 
 
@@ -49,7 +67,7 @@ async def time_task():
     while not client.is_closed():
         now_time = datetime.datetime.now().strftime("%A-%H:%M")
         if now_time == NOTIFY_TIME:
-            msg = f"{EVENT_NAME} 本週售票狀況為：\n{event_ticket_count()}"
+            msg = f"**{EVENT_NAME}** 本週售票狀況：\n{event_ticket_count()}"
             await client.channel.send(msg)
             await asyncio.sleep(60)
         else:
